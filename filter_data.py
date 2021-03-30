0ac4974222c97cf36eb6e59839273d3d5791c35b
XDong18 <liuxd1215@gmail.com>
2021-03-31 02:02:06 +0800

    add filter code and filter train

---
+++ filter_data.py
@@ -0,0 +1,18 @@
+import os
+import os.path as osp
+
+img_dir = '/shared/xudongliu/bdd100k/100k/train'
+label_dir = '/shared/xudongliu/bdd100k/100k/troad_label_train'
+temp_img_dir  = '/shared/xudongliu/bdd100k/100k/train_else/'
+label_names = os.listdir(label_dir)
+num  = 0
+for fn in os.listdir(img_dir):
+    name = fn.split('.')[0]+'.png'
+    if name not in label_names:
+        cmd = 'mv ' + osp.join(img_dir, fn) + ' ' + temp_img_dir
+        num += 1
+        os.system(cmd)
+        print('caught ', fn, num)
+        # exit()
+    
+
